107c097d23dd5d79b819721b7b74bc53554e008b
aberu78 <aberu78@gmail.com>
2020-09-28 10:49:03 -0500

    input code added

--- student/src/Student.java
+++ student/src/Student.java
@@ -1,17 +1,34 @@
 public class Student {
-    String first_name;
-    String last_name;
-    int id;
 
-    public Student(int num_id, String fname, String lname) {
-        first_name = fname;
-        last_name = lname;
-        id = num_id;
+    private String first_name;
+    private String last_name;
+    private int num_id;
+
+
+    public Student(int id, String fname, String lname) {
+       first_name = fname;
+       last_name = lname;
+       num_id = id;
+
     }
-
     public void Print_Student(){
-        System.out.println("ID :" + id + " " + "Name: " + first_name + " " + last_name);
+        System.out.println("insude Print_Student method");
+        System.out.println("ID :" + num_id + " " + "Name: " + first_name + " " + last_name);
     }
 
+    public String getFirst(){
+        return first_name;
+    }
+    public String getLast(){
+        return last_name;
+    }
+    public int getID(){
+        return num_id;
+    }
 
+    public void setStudent(String fname, String lname, int num_id){
+        this.first_name = fname;
+        this.last_name = lname;
+        this.num_id = num_id;
+    }
 }
