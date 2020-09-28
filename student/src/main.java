9956c93b7fac22b8977a98e0a15aea6c1d61e724
aberu78 <aberu78@gmail.com>
2020-09-27 20:33:27 -0500

    main added

--- student/src/main.java
+++ student/src/main.java
@@ -1,2 +1,10 @@
 public class main {
+
+
+    public static void main(String[] args) {
+
+        Student student = new Student(111, "Kaori", "Minami");
+        System.out.println(student.id + student.first_name + student.last_name);
+        student.Print_Student();
+    }
 }
