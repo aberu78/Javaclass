b82fe9c223f28895c7f6133d429b760ca9e87b40
aberu78 <aberu78@gmail.com>
2020-09-30 11:18:23 -0500

    getAvg and printAvg is added

--- student/src/Student.java
+++ student/src/Student.java
@@ -1,20 +1,68 @@
+import java.util.HashMap;
+import java.util.*;
 public class Student {
 
     private String firstName;
     private String lastName;
     private int numID;
-
+    private HashMap<String, ArrayList<Integer>> grades = new HashMap<>();
 
     public Student(int id, String fname, String lname) {
         firstName = fname;
         lastName = lname;
         numID = id;
+    }//end of Student
 
-    }
-    public void PrintStudent(){
-        System.out.println("inside Print_Student()");
+
+    public void printStudent(){
         System.out.println("ID :" + numID + " " + "Name: " + firstName + " " + lastName);
     }
+
+    public void printGrade(){
+        System.out.println("inside printGrade()");
+        if(gradeExist()) {
+            for (String subject : grades.keySet()) {
+                System.out.println("Subject:  " + subject );
+                for (int i : grades.get(subject))
+                    System.out.println("Grades : " + String.valueOf(i));
+            }
+        }//end of if
+        else
+            System.out.println("There is no grade recorded for this student");
+    }//end of printGrade()
+
+
+    public void addGrade(String subject , int point){
+        System.out.println("inside add_grade()");
+        if(!grades.containsKey(subject))
+            grades.put(subject, new ArrayList<>());
+
+        grades.get(subject).add(point);
+    }
+
+    private boolean gradeExist() {
+        return !grades.isEmpty();
+    }
+
+    public int getAvg(String subject){
+        int sum = 0;
+        for (int i : grades.get(subject))
+            sum += i;
+        return sum / grades.get(subject).size();
+
+    }
+
+    public void printAvg(){
+        if(gradeExist()) {
+            for (String subject : grades.keySet()) {
+                System.out.println("Subject:  " + subject);
+                int avg = getAvg(subject);
+                System.out.println(subject + " average is :" + String.valueOf(avg));
+            }
+        }
+        else
+            System.out.println("There is no grade recorded for this student");
+        }//end of printAvg
 
     public String getFirst(){
         return firstName;
@@ -32,3 +80,4 @@ public class Student {
         this.numID = numID;
     }
 }
+
