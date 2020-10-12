904320c264652e301161ffd094f4249769199f27
aberu78 <aberu78@gmail.com>
2020-10-12 11:13:29 -0500

    change code

--- student/src/Student.java
+++ student/src/Student.java
@@ -1,24 +1,22 @@
 import org.json.simple.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
-/**
- *
- */
-public class Student {
+public class Student extends Person {
 
-    private String firstName;
-    private String lastName;
-    private int numID;
-    private final HashMap<String, ArrayList<Integer>> grades = new HashMap<>();
+    private HashMap<String, Subject> grades = new HashMap<>();
 
-    public Student(int id, String fname, String lname) {
-        firstName = fname;
-        lastName = lname;
-        numID = id;
-    }//end of Student
+    public Student(int numID, String firstName, String lastName) {
+        super(numID, firstName, lastName);
+
+    }
+
+    public Student(int numID, String firstName, String lastName, HashMap<String, Subject> grades) {
+        super(numID, firstName, lastName);
+        this.grades = grades;
+    }
+
 
     /***
      * Creates a student from a valid json string
@@ -27,112 +25,97 @@ public class Student {
      */
     public static Student createFromJson(JSONObject studentData)
     {
-        Student result = null;
 
         JSONObject studentDetails = (JSONObject) studentData.get("studentData");
         String firstName = (String) studentDetails.get("firstname");
         String lastName = (String) studentDetails.get("lastname");
         int id = (int) (long) studentDetails.get("id");
 
-        result = new Student(id, firstName, lastName);
         HashMap<String, ArrayList<Long>> studentGrades = (HashMap<String, ArrayList<Long>>) studentDetails.get("subject");
+
+        Student student = new Student(id, firstName, lastName);
 
         if (!studentGrades.isEmpty()) {
             for (String subject : studentGrades.keySet()) {
-                ArrayList<Integer> tmp = new ArrayList<>();
-                ArrayList<Long> gradesList = studentGrades.get(subject);
-                gradesList.forEach(grade -> tmp.add((int) (long) grade));
-                result.addGrades(subject, tmp);
+                student.addGrades(subject, studentGrades.get(subject));
+                System.out.println(subject + " : " + studentGrades.get(subject));
+
             }
         }
-        else
-        {
-            result = null;
-        }
-        return result;
+
+
+        return student;
+
     }
 
     public void printStudent() {
-        System.out.println("ID :" + numID + " " + "Name: " + lastName + " , " + firstName);
-    }
-
-    public void printGrade() {
-        //System.out.println("inside printGrade()");
-        if (gradeExist()) {
-            for (String subject : grades.keySet()) {
-                System.out.println("Subject:  " + subject);
-                for (int i : grades.get(subject))
-                    System.out.println("Grades : " + i);
-            }
-        }//end of if
-        else
-            System.out.println("There is no grade recorded for this student");
-    }//end of printGrade()
-
-    /**
-     * Adds a grade to the student
-     * @param subject The subject to add a grade too.
-     * @param point The grade
-     */
-    public void addGrade(String subject, int point) {
-        if (!grades.containsKey(subject))
-            grades.put(subject, new ArrayList<>());
-
-        grades.get(subject).add(point);
-    }
-
-    public void addGrades(String subject, ArrayList<Integer> gradeList) {
-        grades.put(subject, gradeList);
-    }
-
-    public boolean gradeExist() {
-        return !grades.isEmpty();
-    }
-
-    public int getAvg(String subject) {
-        int sum = 0;
-        for (int i : grades.get(subject))
-            sum += i;
-        return sum / grades.get(subject).size();
-
+        System.out.println("ID :" + getID() + " " + "Name: " + getLast() + " , " + getFirst());
     }
 
     public void printAvg() {
         if (gradeExist()) {
             for (String subject : grades.keySet()) {
                 System.out.println("Subject:  " + subject);
-                int avg = getAvg(subject);
+                long avg = grades.get(subject).getAvg();
                 System.out.println("Average is :" + avg);
             }
         } else
             System.out.println("There is no grade recorded for this student");
     }//end of printAvg
 
-    public String getFirst() {
-        return firstName;
+
+    public  boolean gradeExist() {
+        return !grades.isEmpty();
     }
 
-    public String getLast() {
-        return lastName;
+  /*  public long getAvg(String subject) {
+
+        return getAvg(subject);
+
     }
 
-    public int getID() {
-        return numID;
-    }
+*/
 
     public Set<String> getSubjects() {
         return this.grades.keySet();
     }
 
-    public ArrayList<Integer> getGrade(String subject) {
-        return new ArrayList<>(grades.get(subject));
+
+    public void addGrades(String subject, ArrayList<Long> gradeList) {
+        //build subject then add
+        for (Long aLong : gradeList)
+            addGrade(subject, aLong);
+
     }
 
-    @SuppressWarnings("unused")
-    public void setStudent(String fname, String lname, int numID) {
-        this.firstName = fname;
-        this.lastName = lname;
-        this.numID = numID;
+    public void addGrade(String subject, long point) {
+
+        if (!grades.containsKey(subject)) {
+            Subject aSubject = new Subject(subject);
+            aSubject.addGrade(point);
+            grades.put(subject,aSubject);
+        }
+        else
+            grades.get(subject).addGrade(point);
     }
+
+
+    public ArrayList<Long> getGrade(String subject) {
+        return grades.get(subject).getGrades();
+    }
+
+    public void printGrade() {
+        //System.out.println("inside printGrade()");
+        if (gradeExist()) {
+            for (String subject : grades.keySet()) {
+               grades.get(subject).printGrades();
+            }
+        }
+        else
+            System.out.println("There is no grade recorded for this student");
+    }
+
+
+
 }
 
